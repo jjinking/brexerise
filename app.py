36c53e01b171fcae0cdd13fb56dd4c42da970667
Joseph <jjinking@gmail.com>
2015-11-02 04:36:00 +0900

    Made the app more objected oriented

--- app.py
+++ app.py
@@ -3,58 +3,107 @@ import curses
 import sys
 from datetime import datetime
 
-KEYS_EXIT = {27, ord('q'), ord('Q')}
 
-HEIGHT = 10
-WIDTH = 46
+class Widget:
+
+    def __init__(self, key_action):
+        '''
+        Initialize user input keys to actions
+        key_actions is a dictionary mapping characters to functions
+        '''
+        self.key_action = key_action
+
+    def show(self):
+        '''
+        Must implement this method to display window
+        '''
+        raise NotImplementedError("Method not implemented")
+    show = asyncio.coroutine(show)
+
+    def process_input(self):
+        '''
+        Handle user input key by running corresponding function
+        '''
+        key = self.w.getch()
+        if key in self.key_action:
+            self.key_action[key]()
+
+    def run(self):
+        loop = asyncio.get_event_loop()
+        try:
+            # Add listener for user keyboard input
+            loop.add_reader(sys.stdin, self.process_input)
+            loop.run_until_complete(self.show())
+        finally:
+            loop.close()
 
 
-@asyncio.coroutine
-def main_menu(window):
-    # Set up window
-    window.border(0)
+class Timer(Widget):
 
-    # Add welcome message at the top
-    top_message = "Welcome to Brexercise (Break and Exercise)!"
-    window.addstr(0, WIDTH // 2 - len(top_message) // 2, top_message)
-    window.addstr(2, 2, "Select from one of the following options:")
-    window.addstr(4, 4, "S - Start Timer")
-    window.addstr(5, 4, "Q - Exit Program")
-    while True:
-        window.addstr(
-            8, 2, datetime.now().strftime("%A, %d. %B %Y %I:%M:%S%p"))
-        window.refresh()
-        yield from asyncio.sleep(0.2)
+    HEIGHT = 10
+    WIDTH = 46
 
+    def show(self):
+        BEGIN_Y = (curses.LINES - 1) // 2 - self.HEIGHT // 2
+        BEGIN_X = (curses.COLS - 1) // 2 - self.WIDTH // 2
+        self.w = curses.newwin(self.HEIGHT, self.WIDTH, BEGIN_Y, BEGIN_X)
+        self.w.border(0)
 
-def process_input(window):
-    """
-    User input
-    """
-    key = window.getch()
-    if key in KEYS_EXIT:
-        sys.exit(1)
+        # Add welcome message at the top
+        top_message = "Running Timer"
+        self.w.addstr(
+            0, self.WIDTH // 2 - len(top_message) // 2, top_message)
+        self.w.addstr(2, 2, "Select from one of the following options:")
+        self.w.addstr(4, 4, "S - Start Timer")
+        self.w.addstr(5, 4, "Q - Exit Program")
+        while True:
+            self.w.addstr(
+                8, 2, datetime.now().strftime("%A, %d. %B %Y %I:%M:%S%p"))
+            self.w.refresh()
+            yield from asyncio.sleep(0.2)
 
 
-def main(stdscr):
-    # Invisible cursor
-    curses.curs_set(0)
-    BEGIN_Y = (curses.LINES - 1) // 2 - HEIGHT // 2
-    BEGIN_X = (curses.COLS - 1) // 2 - WIDTH // 2
-    window = curses.newwin(HEIGHT, WIDTH, BEGIN_Y, BEGIN_X)
-    window.nodelay(True)  # Nonblocking
-    loop = asyncio.get_event_loop()
-    try:
-        # Add listener for user keyboard input
-        loop.add_reader(sys.stdin, process_input, window)
-        loop.run_until_complete(main_menu(window))
-    finally:
-        loop.close()
+class MainMenu(Widget):
+
+    HEIGHT = 10
+    WIDTH = 46
+
+    def show(self):
+        BEGIN_Y = (curses.LINES - 1) // 2 - self.HEIGHT // 2
+        BEGIN_X = (curses.COLS - 1) // 2 - self.WIDTH // 2
+        self.w = curses.newwin(self.HEIGHT, self.WIDTH, BEGIN_Y, BEGIN_X)
+        # Window border
+        self.w.border(0)
+
+        # Add welcome message at the top
+        top_message = "Welcome to Brexercise (Break and Exercise)!"
+        self.w.addstr(
+            0, self.WIDTH // 2 - len(top_message) // 2, top_message)
+        self.w.addstr(2, 2, "Select from one of the following options:")
+        self.w.addstr(4, 4, "S - Start Timer")
+        self.w.addstr(5, 4, "Q - Exit Program")
+        while True:
+            self.w.addstr(
+                8, 2, datetime.now().strftime("%A, %d. %B %Y %I:%M:%S%p"))
+            self.w.refresh()
+            yield from asyncio.sleep(0.2)
+
+
+class App:
+    def run(self, stdscr):
+        # Invisible cursor
+        curses.curs_set(0)
+
+        # Show main menu
+        MainMenu({
+            27: lambda: sys.exit(1),
+            ord('q'): lambda: sys.exit(1),
+            ord('Q'): lambda: sys.exit(1)}).run()
 
 
 if __name__ == "__main__":
     try:
-        curses.wrapper(main)
+        curses.wrapper(App().run)
     except KeyboardInterrupt:
         sys.stderr.write("Ctrl-c exit\n\n")
         sys.exit(1)
